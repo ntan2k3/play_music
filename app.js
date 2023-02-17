cca7b17b4ad04b4fadad8d3f26ae706424ded34a
ntan2k3 <an2707123@gmail.com>
2023-02-17 12:01:34 +0700

    Add new project music

--- app.js
+++ app.js
@@ -5,14 +5,27 @@ const prevBtn = document.querySelector('.play-back');
 const image = document.getElementById('image');
 const nameSong = document.getElementById('name');
 const authorSong = document.getElementById('author');
+const durationTime = document.querySelector('.right');
+const remainingTime = document.querySelector('.left');
+const repeatedBtn = document.querySelector('.play_again');
+const widthLine = document.querySelector('.line');
 let isPlaying = true;
 let indexSong = 0;
 
+
 playBtn.addEventListener('click', playPause);
+song.addEventListener("ended", () => {
+    changeSong(1);
+})
 
 nextBtn.addEventListener('click', function() {
     changeSong(1);
 });
+
+repeatedBtn.addEventListener('click', function() {
+    changeSong(0);
+});
+
 prevBtn.addEventListener('click', function() {
     changeSong(-1);
 });
@@ -49,6 +62,10 @@ function changeSong(x) {
             indexSong = musics.length - 1;
         }
         isPlaying = true;
+    } else if (x === 0) {
+        // repeated song
+        repeatedBtn.style.color = 'white';
+        isPlaying = true;
     }
     song.setAttribute("src", `./song/${musics[indexSong]}`);
     image.setAttribute("src", `./image/${images[indexSong]}`);
@@ -56,3 +73,25 @@ function changeSong(x) {
     authorSong.innerHTML = authors[indexSong];
     playPause();
 }
+
+
+function formatTimer(number) {
+    const minutes = Math.floor(number / 60);
+    const seconds = Math.floor(number - minutes * 60);
+    return `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+}
+
+function displayTimer() {
+    const { duration, currentTime } = song;
+    remainingTime.textContent = formatTimer(currentTime);
+    if (!duration) {
+        durationTime.textContent = "0:00";
+    } else {
+        durationTime.textContent = formatTimer(duration);
+    }
+    widthLine.style.width = `${Math.floor(currentTime * 320 / duration)}px`;
+}
+
+displayTimer();
+
+setInterval(displayTimer, 1000);
